3e11d4b7a1a9d9d9139c9899d78af85a4e11735b
cptrok <cptrok@github.com>
2026-05-15 16:38:56 +0900

    날짜 요약 진행내역 - 특이사항 유무 표기 추가

--- pages/app/[...slug].js
+++ pages/app/[...slug].js
@@ -999,7 +999,7 @@ export default function App() {
         <div className="sidebar">
           <div className="sidebar-header">
             <div className="sidebar-top">
-              <span className="sidebar-title">록근_v52</span>
+              <span className="sidebar-title">록근_v53</span>
               {currentTab === 'notes' && <button className="btn-new" onClick={newNote}>+</button>}
             </div>
             <div className="sidebar-tabs">
