58cb63ceea6ad64e983d620e21c5ef6f7a84de43
cptrok <cptrok@github.com>
2026-05-22 16:40:26 +0900

    Clickpad 버전 표시 복원 및 pre-commit 훅 패턴 업데이트

--- pages/app/[...slug].js
+++ pages/app/[...slug].js
@@ -1320,7 +1320,7 @@ export default function App() {
         <div className="sidebar">
           <div className="sidebar-header">
             <div className="sidebar-top">
-              <span className="sidebar-title">Clickpad</span>
+              <span className="sidebar-title">Clickpad_v124</span>
               {currentTab === 'notes' && <button className="btn-new" onClick={newNote}>+</button>}
             </div>
             <div className="sidebar-tabs">
