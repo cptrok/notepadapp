6b40eafa2f9b323bfc9e6ec2558f4870df8d8c17
cptrok <cptrok@github.com>
2026-05-29 10:30:19 +0900

    FAQ 상세 HTML 포맷팅 CSS 추가 - p/br/ul/li/pre/table 등

--- pages/app/[...slug].js
+++ pages/app/[...slug].js
@@ -1630,7 +1630,7 @@ export default function App() {
         <div className="sidebar">
           <div className="sidebar-header">
             <div className="sidebar-top">
-              <span className="sidebar-title">Clickpad_v154</span>
+              <span className="sidebar-title">Clickpad_v155</span>
               {currentTab === 'notes' && <button className="btn-new" onClick={newNote}>+</button>}
             </div>
             <div className="sidebar-tabs">
