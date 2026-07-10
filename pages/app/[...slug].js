2c390923bbcd1f7cd253ac65a736432d504f3394
cptrok <cptrok@github.com>
2026-07-10 14:59:05 +0900

    Vercel 배포 재트리거

--- pages/app/[...slug].js
+++ pages/app/[...slug].js
@@ -2308,7 +2308,7 @@ export default function App() {
         <div className="sidebar">
           <div className="sidebar-header">
             <div className="sidebar-top">
-              <span className="sidebar-title">Clickpad_v331</span>
+              <span className="sidebar-title">Clickpad_v332</span>
               {currentTab === 'notes' && <button className="btn-new" onClick={newNote}>+</button>}
             </div>
             <div className="sidebar-tabs">
