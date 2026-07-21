7718eea1e49cc3e6429173182e5786915564b161
cptrok <cptrok@github.com>
2026-07-21 14:11:47 +0900

    [Clickpad_v354] Vercel 재배포 트리거

--- pages/app/[...slug].js
+++ pages/app/[...slug].js
@@ -2495,7 +2495,7 @@ export default function App() {
         <div className="sidebar">
           <div className="sidebar-header">
             <div className="sidebar-top">
-              <span className="sidebar-title">Clickpad_v353</span>
+              <span className="sidebar-title">Clickpad_v354</span>
               {currentTab === 'notes' && <button className="btn-new" onClick={newNote}>+</button>}
             </div>
             <div className="sidebar-tabs">
